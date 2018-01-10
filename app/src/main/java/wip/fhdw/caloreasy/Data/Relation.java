ed9d11ab491e52e7db02ee46be8bf3b573b63129
Falk <falk.leonhard@hotmail.de>
2018-01-10 16:26:54 +0100

    Added Data Entities.

---
+++ app/src/main/java/wip/fhdw/caloreasy/Data/Relation.java
@@ -0,0 +1,42 @@
+package wip.fhdw.caloreasy.Data;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by Falk on 10.01.2018.
+ */
+
+@Entity(primaryKeys = {"grocery", "measurement"}, foreignKeys = {@ForeignKey(entity = Grocery.class,
+        parentColumns = "name", childColumns = "grocery"), @ForeignKey(entity = Measurement.class,
+        parentColumns = "longform", childColumns = "measurement")})
+public class Relation {
+    private String grocery;
+    private String measurement;
+    private int amount;
+
+    public String getGrocery() {
+        return grocery;
+    }
+
+    public void setGrocery(String grocery) {
+        this.grocery = grocery;
+    }
+
+    public String getMeasurement() {
+        return measurement;
+    }
+
+    public void setMeasurement(String measurement) {
+        this.measurement = measurement;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
