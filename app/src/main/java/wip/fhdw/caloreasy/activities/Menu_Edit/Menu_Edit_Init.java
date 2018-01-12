6d2939b3b5db1061ff1570308c2c0c34e283f796
Eliminator8000 <simon_2467@hotmail.de>
2018-01-12 11:52:00 +0100

    SS - custom actionbar

--- app/src/main/java/wip/fhdw/caloreasy/activities/Menu_Edit/Menu_Edit_Init.java
+++ app/src/main/java/wip/fhdw/caloreasy/activities/Menu_Edit/Menu_Edit_Init.java
@@ -3,6 +3,7 @@ package wip.fhdw.caloreasy.activities.Menu_Edit;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -15,19 +16,19 @@ public class Menu_Edit_Init extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_edit);
 
-/*
         // Inflate the custom view and add click handlers for the buttons
-        View actionBarButtons = inflater.inflate(R.layout.edit_event_custom_actionbar,
-                new LinearLayout(mContext), false);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View actionBarButtons = inflater.inflate(R.layout.edit_custom_actionbar,
+                new LinearLayout(this), false);
 
         View cancelActionView = actionBarButtons.findViewById(R.id.action_cancel);
-        cancelActionView.setOnClickListener(mActionBarListener);
+        //cancelActionView.setOnClickListener(mActionBarListener);
 
         View doneActionView = actionBarButtons.findViewById(R.id.action_done);
-        doneActionView.setOnClickListener(mActionBarListener);
+        //doneActionView.setOnClickListener(mActionBarListener);
 
         // Retrieve an instance of the Activity's ActionBar
-        ActionBar actionBar = mContext.getActionBar();
+        ActionBar actionBar = this.getSupportActionBar();
 
         // Hide the icon, title and home/up button
         actionBar.setDisplayShowHomeEnabled(false);
@@ -38,7 +39,6 @@ public class Menu_Edit_Init extends AppCompatActivity {
         actionBar.setCustomView(actionBarButtons);
         actionBar.setDisplayShowCustomEnabled(true);
 
-*/
     }
 
 
