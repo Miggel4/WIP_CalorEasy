41622625d0e3a1918339c46ef836e1a2c07e9e52
Falk <falk.leonhard@hotmail.de>
2018-02-04 13:25:44 +0100

    Added Grocery Activity.

---
+++ app/src/main/java/wip/fhdw/caloreasy/activities/Grocery_View/Grocery_View_ApplicationLogic.java
@@ -0,0 +1,18 @@
+package wip.fhdw.caloreasy.activities.Grocery_View;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Falk on 03.02.2018.
+ */
+
+public class Grocery_View_ApplicationLogic {
+
+    private Grocery_View_GUI mGUI;
+    AppCompatActivity mActivity;
+
+    public Grocery_View_ApplicationLogic(Grocery_View_GUI gui, AppCompatActivity activity){
+        mGUI = gui;
+        mActivity = activity;
+    }
+}
---
+++ app/src/main/java/wip/fhdw/caloreasy/activities/Grocery_View/Grocery_View_GUI.java
@@ -0,0 +1,14 @@
+package wip.fhdw.caloreasy.activities.Grocery_View;
+
+import wip.fhdw.caloreasy.R;
+
+/**
+ * Created by Falk on 03.02.2018.
+ */
+
+public class Grocery_View_GUI {
+
+    public Grocery_View_GUI(Grocery_View_Init activity){
+        activity.setContentView(R.layout.activity_grocery__view);
+    }
+}
