f9b7d9d7c8c0022cb3904a3e75794c4e1ede1e3c
Eliminator8000 <simon_2467@hotmail.de>
2018-02-05 18:46:08 +0100

    SS - NavDrawerListener update & Constants for Bundle handling

---
+++ app/src/main/java/wip/fhdw/caloreasy/datastructures/Constants.java
@@ -0,0 +1,11 @@
+package wip.fhdw.caloreasy.datastructures;
+
+/**
+ * Created by Simon on 03.02.2018.
+ */
+
+public class Constants {
+
+    public static final String KEYMENUEDITNAME = "K1";
+
+}
